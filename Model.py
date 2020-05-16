f4a135b530a5891c319cb861c3d8b5701a3e73d0
Reynaldi <38524172+reynaldiRepo@users.noreply.github.com>
2020-05-16 21:17:11 +0700

    create model

---
+++ Model.py
@@ -0,0 +1,40 @@
+from LocalBinaryPattern import LocalBinaryPattern
+from sklearn.svm import LinearSVC
+from imutils import paths
+import cv2
+import os
+import json
+
+
+class Model():
+    def __init__(self):
+        #initialize LBP descrtiptor 
+        desc = LocalBinaryPattern(24, 8)
+        data = []
+        label = []
+        #inizialze training path
+        dirpath = (os.path.dirname(__file__))
+        training_rel_path = "training"
+        abs_training_path = os.path.join(dirpath, training_rel_path)
+        #describe image to lbp histogram
+        datasetArr = []
+        for imagePath in paths.list_images(abs_training_path):
+            image = cv2.imread(imagePath)
+            grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY);
+            hist = desc.describe(grey)
+            jsonTemp =  {"dir":imagePath, 
+                        "filename":os.path.basename(imagePath), 
+                        "class":imagePath.split('\\')[-2]}
+            datasetArr.append(jsonTemp)
+            data.append(hist)
+            label.append(jsonTemp["class"])
+
+        #create model
+        modelLSVC = LinearSVC()
+        self.model = modelLSVC.fit(data, label)
+
+        #write to json file
+        datasetJson = json.dumps(datasetArr,indent=4)
+        file = open("dataset.json", "w")
+        file.write(datasetJson)
+        file.close()
